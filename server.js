9da96622e7eccfd40ad08e2af81c57ccc3d25d54
najwanan <theunnamedrd@gmail.com>
2022-09-04 23:46:15 -0700

    pulling in connection string to server.js

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const multer = require('multer')
+const mongoose = require('mongoose')
+const connectDB = require('./config/databse')
+
+
+require('dotenv').config({ path: "./config/.env" });
+
+
+connectDB();
+//pull in router 
+
+
+
+//middleware 
