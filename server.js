27dc489eb4f8d66effa72587cb4869159e57bc04
najwanan <theunnamedrd@gmail.com>
2022-09-04 23:57:11 -0700

    adding middleware for multer

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const multer = require('multer')
 const mongoose = require('mongoose')
 const connectDB = require('./config/databse')
 const fs = require('fs')
+const multer = require("multer");
 
 
 require('dotenv').config({ path: "./config/.env" });
@@ -19,3 +20,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
+
+
+// Step 5 - set up multer for storing uploaded files
+
+
+
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage });
